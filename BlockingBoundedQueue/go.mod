204185d9a64258832e31c0d0c10270caa37f14e7
saurav.k <saurav.k@tallysolutions.com>
2025-08-26 01:53:33 +0530

    [ID-2] Implemented Blocking Bounded Queue in Go
    - Understood diff b/w signal and broadcast
    - Implmented Put,Take methods of BlockingQueue Type struct
    - Used empty interfaces
    - Used sync-Mutex,Cond,WaitGroup an Go routines to test it out
    - You can use channel to wait for go routines as well

---
+++ BlockingBoundedQueue/go.mod
@@ -0,0 +1,3 @@
+module blocking-queue
+
+go 1.25.0
---
+++ BlockingBoundedQueue/main.go
@@ -0,0 +1,123 @@
+﻿package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+type BlockingQueue struct {
+	m    sync.Mutex
+	c    sync.Cond
+	data []interface{}
+	size int
+}
+
+func NewBlockingQueue(capacity int) *BlockingQueue {
+	q := new(BlockingQueue)
+	q.c = sync.Cond{L: &q.m}
+	q.size = capacity
+	return q
+}
+
+// methods
+func (q *BlockingQueue) Put(item interface{}) {
+	q.c.L.Lock()
+	defer q.c.L.Unlock()
+
+	for q.isFull() {
+		q.c.Wait()
+	}
+
+	q.data = append(q.data, item)
+	q.c.Broadcast()
+}
+
+func (q *BlockingQueue) Take() interface{} {
+	q.c.L.Lock()
+	defer q.c.L.Unlock()
+
+	for q.isEmpty() {
+		q.c.Wait()
+	}
+
+	result := q.data[0]
+	q.data = q.data[1:len(q.data)]
+	q.c.Broadcast()
+	return result
+}
+
+func (q *BlockingQueue) isFull() bool {
+	return q.size == len(q.data)
+}
+
+func (q *BlockingQueue) isEmpty() bool {
+	return len(q.data) == 0
+}
+
+func main() {
+	fmt.Println("This is the prototype of Blocking Bounded Queue using mutex and cond in Go")
+
+	//using go routines
+
+	bq := NewBlockingQueue(1)
+	var wg sync.WaitGroup
+
+	// Producers
+	for i:=0; i<10; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			bq.Put("A")
+		}()
+
+		
+	}
+
+	// Consumers
+	for i:=0; i<10; i++ {
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			val := bq.Take() // blocking call
+			fmt.Printf("Got %v %v\n", val, i)
+		}(i)
+	}
+
+	wg.Wait()
+	
+	
+	/*
+	bq := NewBlockingQueue(1)
+	done := make(chan bool)
+
+	// slow writer
+	go func() {
+		bq.Put("A")
+		time.Sleep(1000 * time.Millisecond)
+		bq.Put("B")
+		time.Sleep(1000 * time.Millisecond)
+		bq.Put("C")
+	}()
+
+	// reader will be blocked
+	go func() {
+		item := bq.Take()
+		fmt.Printf("Got %v\n", item)
+		item = bq.Take()
+		fmt.Printf("Got %v\n", item)
+		item = bq.Take()
+		fmt.Printf("Got %v\n", item)
+		done <- true
+	}()
+
+	// block while done
+	<-done*/
+
+}
+/*
+🔹 Key Takeaway
+
+-> Signal() is fine if you can guarantee only one kind of waiter (e.g., one consumer thread waiting for items).
+
+-> With multiple producers and consumers, you can’t guarantee the signal wakes the right type → use Broadcast().
+*/
